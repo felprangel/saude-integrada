153110574ce24be9594b63d76db3ec3ad7c2ac4f
felprangel <feliperangel897@gmail.com>
2025-05-25 13:10:40 -0300

    ajusta configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { GlobalStyle } from '@/styles/globals'
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Saúde Integrada</title>
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
 }
