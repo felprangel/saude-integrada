6d2b74605e07bdb46740597c208a34def861df60
felprangel <feliperangel897@gmail.com>
2025-05-25 13:04:39 -0300

    adiciona estilização global

---
+++ src/styles/globals.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    // Adicionar variáveis de cor aqui
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  body {
+    min-height: 100vh;
+    background-color: var(--gray);
+  }
+
+  input,
+  textarea,
+  button {
+    font: 400 1rem 'Poppins', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+    border-radius: 0.1rem;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+`;
