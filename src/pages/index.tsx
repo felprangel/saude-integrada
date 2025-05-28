6d3877a232f3375366a4a70cc84fd542c7eb4b84
felprangel <feliperangel897@gmail.com>
2025-05-27 22:05:05 -0300

    adiciona estilização da hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
 import { Header } from '@/components/Header'
+import Hero from '../../public/hero.png'
+import Image from 'next/image'
+import styled from 'styled-components'
 
 export default function Home() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <HeroSection>
+        <Paragraph></Paragraph>
+        <Image src={Hero} alt="hero image" />
+      </HeroSection>
+    </>
+  )
 }
+
+const HeroSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const Paragraph = styled.div``
