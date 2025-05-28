9dcacaccd7540bf8f69b3e75dd79aa92b20cb28c
felprangel <feliperangel897@gmail.com>
2025-05-27 21:40:14 -0300

    adiciona componente de header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <p>Teste</p>
+    </HeaderContainer>
+  )
+}
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  background-color: var(--primary-color);
+`
