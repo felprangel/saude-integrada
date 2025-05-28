76202eb9b50378cd96eff5850a79cfa86a94f7a0
felprangel <feliperangel897@gmail.com>
2025-05-27 21:45:54 -0300

    adiciona logo no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image'
+import Logo from '../../../public/logo.svg'
 import styled from 'styled-components'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <p>Teste</p>
+      <LogoContainer>
+        <Image src={Logo} alt="logo" />
+      </LogoContainer>
     </HeaderContainer>
   )
 }
@@ -11,4 +15,8 @@ export function Header() {
 const HeaderContainer = styled.div`
   width: 100%;
   background-color: var(--primary-color);
+`
+
+const LogoContainer = styled.div`
+  background-color: var(--grey);
 `
