19e88e39ac1677bc93bf96a3fe0cb9c5377ff167
felprangel <feliperangel897@gmail.com>
2025-05-27 22:43:57 -0300

    ajusta estilização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@/components/Header'
 import Hero from '../../public/hero.png'
+import ClickIcon from '../../public/icons/click.svg'
 import Image from 'next/image'
 import styled from 'styled-components'
 
@@ -8,7 +9,13 @@ export default function Home() {
     <>
       <Header />
       <HeroSection>
-        <Paragraph></Paragraph>
+        <Paragraph>
+          <HeroTitle>Monitoramento participativo para uma saúde única</HeroTitle>
+          <HeroText>Conectando dados, ciência e comunidade para fortalecer a saúde pública</HeroText>
+          <button>
+            Acessar painel de dados <Image src={ClickIcon} alt="click icon" />
+          </button>
+        </Paragraph>
         <Image src={Hero} alt="hero image" />
       </HeroSection>
     </>
@@ -21,4 +28,17 @@ const HeroSection = styled.div`
   width: 100%;
 `
 
-const Paragraph = styled.div``
+const Paragraph = styled.div`
+  margin: 3rem;
+  width: 40%;
+`
+
+const HeroTitle = styled.h3`
+  font-weight: 500;
+  font-size: 2rem;
+`
+
+const HeroText = styled.p`
+  font-weight: 300;
+  font-size: 1.2rem;
+`
