f8c40076b0dcaaeda4086a07b8dfdb2b586c3d32
felprangel <feliperangel897@gmail.com>
2025-06-01 18:38:16 -0300

    adiciona componente de container main

---
+++ src/components/MainContainer/index.tsx
@@ -0,0 +1,10 @@
+import { HTMLProps } from 'react'
+import styled from 'styled-components'
+
+export function MainContainer(props: HTMLProps<HTMLDivElement>) {
+  return <Container>{props.children}</Container>
+}
+
+const Container = styled.div`
+  padding: 5rem 4rem;
+`
