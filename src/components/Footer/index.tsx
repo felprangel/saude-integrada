1fe049014f5d2b4f1cc168e0a16fa346a4d3f4b2
felprangel <feliperangel897@gmail.com>
2025-06-01 10:41:08 -0300

    adiciona componente de footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export function Footer() {
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <Title>Links rápidos</Title>
+        <Link>Painel</Link>
+        <Link>Notificar</Link>
+        <Link>Educação</Link>
+        <Link>Repositório</Link>
+      </FooterContent>
+      <FooterContent></FooterContent>
+      <FooterContent></FooterContent>
+    </FooterContainer>
+  )
+}
+
+const FooterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--primary-color);
+`
+
+const FooterContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: var(--grey);
+`
+
+const Title = styled.h3`
+  color: var(--grey);
+`
+
+const Link = styled.a``
