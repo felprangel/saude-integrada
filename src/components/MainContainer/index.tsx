65bf841dd4167f2ffa952fd5ab8a91705997bf7e
felprangel <feliperangel897@gmail.com>
2025-06-01 20:17:49 -0300

    adiciona props no main container

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -2,7 +2,7 @@ import { HTMLProps } from 'react'
 import styled from 'styled-components'
 
 export function MainContainer(props: HTMLProps<HTMLDivElement>) {
-  return <Container>{props.children}</Container>
+  return <Container {...props}>{props.children}</Container>
 }
 
 const Container = styled.div`
