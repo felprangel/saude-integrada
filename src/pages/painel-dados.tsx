7f04e4314762f096d1409cd2da64a43b650e497a
felprangel <feliperangel897@gmail.com>
2025-06-01 12:33:29 -0300

    inclui componente de paragrafo na página de painel de dados

--- src/pages/painel-dados.tsx
+++ src/pages/painel-dados.tsx
@@ -1,5 +1,6 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { Paragraph } from '@/components/Paragraph'
 import { Title } from '@/components/Title'
 
 export default function PainelDados() {
@@ -7,6 +8,10 @@ export default function PainelDados() {
     <>
       <Header />
       <Title>Painel de indicadores epidemiológicos</Title>
+      <Paragraph>
+        contextualizar que os dados apresentados são atualizados periodicamente e têm como objetivo apoiar a gestão
+        pública e a conscientização comunitária
+      </Paragraph>
       <Footer />
     </>
   )
