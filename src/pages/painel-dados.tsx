b8a579b2a56b82b235f00a390450ccb2de87f4cc
felprangel <feliperangel897@gmail.com>
2025-06-01 12:05:38 -0300

    utiliza componente de título

--- src/pages/painel-dados.tsx
+++ src/pages/painel-dados.tsx
@@ -1,10 +1,12 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { Title } from '@/components/Title'
 
 export default function PainelDados() {
   return (
     <>
       <Header />
+      <Title>Painel de indicadores epidemiológicos</Title>
       <Footer />
     </>
   )
