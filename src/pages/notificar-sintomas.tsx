ae1b81527463dbfc2b66f23f25949a773b5a65a0
felprangel <feliperangel897@gmail.com>
2025-06-01 20:18:05 -0300

    adiciona inicio da página de notificar sintoma

--- src/pages/notificar-sintomas.tsx
+++ src/pages/notificar-sintomas.tsx
@@ -1,11 +1,34 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { MainContainer } from '@/components/MainContainer'
+import { Paragraph } from '@/components/Paragraph'
+import { Title } from '@/components/Title'
+import Image from 'next/image'
+import Notify from '../../public/notify.svg'
+import styled from 'styled-components'
 
 export default function NotificarSintomas() {
   return (
     <>
       <Header />
+      <MainContainer style={{ paddingTop: 0 }}>
+        <TitleContainer>
+          <div>
+            <Title>Notificar sintomas</Title>
+            <Paragraph style={{ maxWidth: '100%' }}>
+              Ajude a monitorar sua comunidade notificando sintomas de doenças. Suas informações serão tratadas de forma
+              segura e anônima
+            </Paragraph>
+          </div>
+          <Image src={Notify} alt="notify-icon" />
+        </TitleContainer>
+      </MainContainer>
       <Footer />
     </>
   )
 }
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
