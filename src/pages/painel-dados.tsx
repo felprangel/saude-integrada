11249c67e105dc43aa33487556f38be9abde7e89
felprangel <feliperangel897@gmail.com>
2025-06-01 11:12:34 -0300

    renomeia componente

--- src/pages/painel-dados.tsx
+++ src/pages/painel-dados.tsx
@@ -1,7 +1,7 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
-export default function PaineilDados() {
+export default function PainelDados() {
   return (
     <>
       <Header />
