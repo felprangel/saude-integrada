c9a2e5c33f6c3ed9ed74e74a210fd617bde52106
felprangel <feliperangel897@gmail.com>
2025-06-02 22:27:35 -0300

    adiciona titulo do repositorio cientifico

--- src/pages/repositorio-cientifico.tsx
+++ src/pages/repositorio-cientifico.tsx
@@ -1,10 +1,20 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { MainContainer } from '@/components/MainContainer'
+import { Paragraph } from '@/components/Paragraph'
+import { Title } from '@/components/Title'
 
 export default function RepositorioCientifico() {
   return (
     <>
       <Header />
+      <MainContainer>
+        <Title>Repositório científico</Title>
+        <Paragraph>
+          Acesse cartilhas, artigos científicos, boletins informativos e documentos produzidos pelo observatório e
+          parceiros do projeto
+        </Paragraph>
+      </MainContainer>
       <Footer />
     </>
   )
