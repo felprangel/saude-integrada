1eac50b9b0ae9e3431a2533ba29ca99c5c4ad591
felprangel <feliperangel897@gmail.com>
2025-06-04 19:17:59 -0300

    adiciona seção de título

--- src/pages/mapa-saude.tsx
+++ src/pages/mapa-saude.tsx
@@ -1,10 +1,20 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { MainContainer } from '@/components/MainContainer'
+import { Paragraph } from '@/components/Paragraph'
+import { Title } from '@/components/Title'
 
 export default function MapaSaude() {
   return (
     <>
       <Header />
+      <MainContainer>
+        <Title>Mapa regional de saúde</Title>
+        <Paragraph>
+          Visualize no mapa a distribuição geográfica dos casos notificados. Acompanhe onde estão os focos por município
+          ou bairro e filtre por tipo de doença.
+        </Paragraph>
+      </MainContainer>
       <Footer />
     </>
   )
